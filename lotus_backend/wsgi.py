3e9e2df9b99388bc6df05693446aa062219336fa
tventura97 <tventura0297@gmail.com>
2019-09-15 16:17:49 -0400

    refactored project

--- lotus_backend/wsgi.py
+++ lotus_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysustainablejourney_backend project.
+WSGI config for mysustainlotusablejourney_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysustainablejourney_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lotus_backend.settings')
 
 application = get_wsgi_application()

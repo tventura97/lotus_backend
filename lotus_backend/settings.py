077210aaf1c9b4d8e416b1d4df69b08abbcbad62
tventura97 <tventura0297@gmail.com>
2019-09-15 17:47:39 -0400

    updated urlpatterns

--- lotus_backend/settings.py
+++ lotus_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ChallengeManager.apps.ChallengemanagerConfig',
+    'AccountManager.apps.AccountManagerConfig',
     'rest_framework'
 ]
 
